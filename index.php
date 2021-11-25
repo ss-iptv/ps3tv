d069fb1d3b5884dd849a456b3cd2a8553246f825
elthondsaraujo <78942062+elthondsaraujo@users.noreply.github.com>
2021-11-25 15:14:27 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -1,16 +1,1 @@
-﻿#EXTM3U
-
-#EXTINF:-1,Pluto TV - Mundo
-m3u:https://raw.githubusercontent.com/matthuisman/i.mjh.nz/master/PlutoTV/all.m3u8
-
-#EXTINF:-1,Pluto TV - Brazil
-m3u:https://alfaconcursos.herokuapp.com/m3u/listas/plutotv.m3u
-
-
-#EXTINF:-1,Canais de TV
-m3u:https://alfaconcursos.herokuapp.com/m3u/listas/iptv-link.m3u
-
-
-#EXTINF:-1,Canais 24H
-m3u:https://alfaconcursos.herokuapp.com/m3u/listas/iptv-24.m3u
-
+﻿
